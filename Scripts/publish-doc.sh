abac4601b56a0a1433d635308da5820c59b33a47
Łukasz Śliwiński <sliwinski.lukas@gmail.com>
2020-06-26 12:11:34 +0200

    Fix the publish documentation script

--- Scripts/publish-doc.sh
+++ Scripts/publish-doc.sh
@@ -8,13 +8,15 @@ if [ -d "~/.observable-kit-documentation/" ]; then rm -rf ~/.observable-kit-docu
 mkdir -p ~/.observable-kit-documentation/ && rsync -r --exclude '.git' ./Documentation/ ~/.observable-kit-documentation/
 
 git checkout gh-pages
-git rm -rf .
+git rm -rf *
 git clean -fxd
 
 cp -r ~/.observable-kit-documentation/ .
 
 rm -rf ~/.observable-kit-documentation/
 
+git add *
+git status
 git commit -m "Publish documentation"
 git push origin gh-pages
 git checkout master
